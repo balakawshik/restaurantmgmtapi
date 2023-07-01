91e8ba67317bbdfd0035ba165d7c44de2e86f94e
Balakawshik B <balakawshik2000@gmail.com>
2023-07-01 08:13:56 +0530

    Change done to get IP & publish in response of GET

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,29 +1,11 @@
 const Restaurant = require("../models/restaurants");
-const { networkInterfaces } = require('os');
+const IP = require('ip');
 
 exports.getRestaurants = function (req, res) {
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-        if (net.family === familyV4Value && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-
   res.status(200).json({
     success: true,
     message: "Welcome to the Restaurants APIs",
-    hostip: results["en0"][0]
+    hostip: IP.address(),
   });
 };
 
@@ -37,12 +19,14 @@ exports.createRestaurant = async (req, res, next) => {
 };
 
 exports.updateRestaurant = async (req, res, next) => {
-    console.log(req.params.name);
-    const restaurant = await Restaurant.updateOne( {name:req.params.name} ,req.body);
-    res.status(200).json({
-      success: true,
-      message: "Restaurant Modified",
-      data: restaurant,
-    });
-  };
-  
+  console.log(req.params.name);
+  const restaurant = await Restaurant.updateOne(
+    { name: req.params.name },
+    req.body
+  );
+  res.status(200).json({
+    success: true,
+    message: "Restaurant Modified",
+    data: restaurant,
+  });
+};
